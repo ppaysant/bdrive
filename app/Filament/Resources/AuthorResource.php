09fb2077d835243e930eea48133ef7447127658c
Patrick Paysant <ppa.work@gmail.com>
2024-08-07 21:53:37 +0200

    [DEV] - Basic lists for authors and series

---
+++ app/Filament/Resources/AuthorResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AuthorResource\Pages;
+use App\Filament\Resources\AuthorResource\RelationManagers;
+use App\Models\Author;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AuthorResource extends Resource
+{
+    protected static ?string $model = Author::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('firstname')
+                    ->searchable()
+                    ->label('Firstname'),
+                TextColumn::make('lastname')
+                    ->searchable()
+                    ->label('Lastname')
+                    ->sortable(),
+                TextColumn::make('albums_count')
+                    ->label('Albums')
+                    ->counts('albums')
+                    ->sortable(),
+            ])
+            ->defaultSort('lastname', 'asc')
+            ->persistSortInSession()
+            ->persistSearchInSession()
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAuthors::route('/'),
+            'create' => Pages\CreateAuthor::route('/create'),
+            'edit' => Pages\EditAuthor::route('/{record}/edit'),
+        ];
+    }
+}
